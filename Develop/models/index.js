4b0a781cb872c524569756fe4f05c8f8aee04133
geigea84 <geigea84@gmail.com>
2020-12-06 16:09:37 -0600

    finished associations in models/index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,17 +4,35 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+//https://sequelize.org/v5/manual/associations.html
+
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongsToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: "product_tag_info",
+  foreignKey: "product_id"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: "product_tag_info",
+  foreignKey: "product_id"
+});
 
 module.exports = {
   Product,
   Category,
   Tag,
-  ProductTag,
+  ProductTag
 };
